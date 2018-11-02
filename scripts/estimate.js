bb82f6948234b706aafd645014967a9c8dfcd650
Erik Bartoš <erik.bartos@gmail.com>
2018-11-02 22:12:17 +0100

    Controller

--- scripts/estimate.js
+++ scripts/estimate.js
@@ -21,3 +21,31 @@ app.controller('Ctrl', function ($scope, $translate) {
 		$translate.use(key);
 	};
 });
+
+app.controller('TableController', ['$scope', function($scope) {
+	var self = this;
+	self.user={id:null,username:'',address:'',email:''};
+	self.id = 4;
+	 
+	self.users = [// In future posts, we will get it from server using service
+			{id:1, username: 'Sam', address: 'NY', email: 'sam@abc.com'},
+			{id:2, username: 'Tomy', address: 'ALBAMA', email: 'tomy@abc.com'},
+			{id:3, username: 'kelly', address: 'NEBRASKA', email: 'kelly@abc.com'}
+	];
+	 
+	self.submit = function() {
+		if(self.user.id === null){
+			self.user.id = self.id++;
+			console.log('Saving New User', self.user);    
+			self.users.push(self.user);//Or send to server, we will do it in when handling services
+		}else{
+			for(var i = 0; i < self.users.length; i++){
+				if(self.users[i].id === self.user.id) {
+				  self.users[i] = self.user;
+				  break;
+				}
+			}
+		   console.log('User updated with id ', self.user.id);
+		}
+	};
+}]);
