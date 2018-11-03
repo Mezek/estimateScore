e6ea804b8df08b8557734aa39889c5af784b0f36
Erik Bartoš <erik.bartos@gmail.com>
2018-11-03 13:39:20 +0100

    table columns

--- scripts/estimate.js
+++ scripts/estimate.js
@@ -28,11 +28,24 @@ app.controller('TableController', ['$scope', function($scope) {
 	var self = this;
 	self.user={id:null,username:'',address:'',email:''};
 	self.id = 4;
+	self.team={tid:null,club:'',z:'',v:'',r:'',p:'',sda:'',sdo:''};
+	self.tid = 9;
 	 
 	self.users = [// In future posts, we will get it from server using service
 			{id:1, username: 'Sam', address: 'NY', email: 'sam@abc.com'},
 			{id:2, username: 'Tomy', address: 'ALBAMA', email: 'tomy@abc.com'},
 			{id:3, username: 'kelly', address: 'NEBRASKA', email: 'kelly@abc.com'}
+	];
+	
+	self.teams = [
+		{tid:1, club: 'ŠK Slovan Bratislava futbal', z: '9', v: '9', r: '0', p: '0', sda: '84', sdo: '7'},
+		{tid:2, club: 'FC Union Nové Zámky', z: '9', v: '7', r: '1', p: '1', sda: '64', sdo: '8'},
+		{tid:3, club: 'Spartak Myjava', z: '9', v: '6', r: '1', p: '2', sda: '40', sdo: '11'},
+		{tid:4, club: 'FC Spartak Trnava', z: '9', v: '4', r: '1', p: '4', sda: '25', sdo: '19'},
+		{tid:5, club: 'FC Nitra', z: '9', v: '4', r: '0', p: '5', sda: '29', sdo: '33'},
+		{tid:6, club: 'FK DAC 1904 Dunajská Streda', z: '9', v: '2', r: '1', p: '6', sda: '20', sdo: '61'},
+		{tid:7, club: 'FK Senica', z: '9', v: '2', r: '0', p: '7', sda: '6', sdo: '60'},
+		{tid:8, club: 'NMŠK 1922 Bratislava', z: '9', v: '0', r: '0', p: '9', sda: '0', sdo: '69'},
 	];
 	 
 	self.submit = function() {
